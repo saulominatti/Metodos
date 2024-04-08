53f2ad1ce7dd1c58b27fc781da8eb39e97921f41
saulominatti <163562450+saulominatti@users.noreply.github.com>
2024-04-07 21:34:29 -0300

    Add files via upload
    Atualização pq tava TUDO ERRADO MEU DEUS

--- NumericalAnalysis/src/NumericalAnalysis.jl
+++ NumericalAnalysis/src/NumericalAnalysis.jl
@@ -1,6 +1,5 @@
-
 module NumericalAnalysis
-include("Includes.jl")
-export round_sum, round_mul, trunc_sum, trunc_mul, absolute_error, relative_error, approximated_value_interval_given_relative_error, bisection,
-fixed_point, newton, secant
+
+greet() = print("Hello World!")
+
 end # module NumericalAnalysis
--- src/Includes.jl
+++ src/Includes.jl
@@ -1,1 +1,2 @@
 include("ErrorAnalysis/ErrorAnalysis.jl")
+include("RootFinding/RootFinding.jl")
